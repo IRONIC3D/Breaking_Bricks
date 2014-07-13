b3b95e99213f1345bd619ff7abba5e85560d33f9
Iyad Horani <i3d@ironic3d.com>
2014-07-14 08:12:05 +1000

    Added Player Controls

--- BreakingBricks/MyScene.m
+++ BreakingBricks/MyScene.m
@@ -8,7 +8,45 @@
 
 #import "MyScene.h"
 
+@interface MyScene ()
+
+@property (nonatomic) SKSpriteNode *paddle;
+
+@end
+
 @implementation MyScene
+
+- (void)addBall:(CGSize)size {
+	// Create a new sprite node from an image
+	SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+	
+	// Position the ball midway through the screen
+	CGPoint myPoint = CGPointMake(size.width / 2, size.height / 2);
+	ball.position = myPoint;
+	
+	ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width / 2];
+	ball.physicsBody.friction = 0;
+	ball.physicsBody.linearDamping = 0;
+	ball.physicsBody.restitution = 1;
+	
+	// Add the sprite node to the scene
+	[self addChild:ball];
+	
+	// create the vector
+	CGVector ballKick = CGVectorMake(12, 12);
+	// apply the vector
+	[ball.physicsBody applyImpulse:ballKick];
+}
+
+- (void)addPlayer:(CGSize)size {
+	// Create paddle sprite
+	self.paddle = [SKSpriteNode spriteNodeWithImageNamed:@"paddle"];
+	self.paddle.position = CGPointMake(size.width / 2, 100);
+	self.paddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddle.frame.size];
+	self.paddle.physicsBody.dynamic = NO;
+	
+	[self addChild:self.paddle];
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
@@ -19,29 +57,28 @@
 		self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
 		
 		// change gravity settings of the physics world
-		self.physicsWorld.gravity = CGVectorMake(0, 0);
+		self.physicsWorld.gravity = CGVectorMake(0, -0.2);
 		
-		// Create a new sprite node from an image
-		SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
-		
-		// Position the ball midway through the screen
-		CGPoint myPoint = CGPointMake(size.width / 2, size.height / 2);
-		ball.position = myPoint;
-		
-		ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width / 2];
-		ball.physicsBody.friction = 0;
-		ball.physicsBody.linearDamping = 0;
-		ball.physicsBody.restitution = 1;
-		
-		// Add the sprite node to the scene
-		[self addChild:ball];
-		
-		// create the vector
-		CGVector ballKick = CGVectorMake(20, 20);
-		// apply the vector
-		[ball.physicsBody applyImpulse:ballKick];
+		[self addBall:size];
+		[self addPlayer:size];
     }
     return self;
+}
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+	for (UITouch *touch in touches) {
+		CGPoint location = [touch locationInNode:self];
+		CGPoint newPosition = CGPointMake(location.x, 100);
+		
+		// stop the paddle from going too far
+		if (newPosition.x < self.paddle.size.width / 2) {
+			newPosition.x = self.paddle.size.width / 2;
+		}
+		if(newPosition.x > (self.size.width - self.paddle.size.width / 2)) {
+			newPosition.x = (self.size.width - self.paddle.size.width / 2);
+		}
+		
+		self.paddle.position = newPosition;
+	}
 }
 
 -(void)update:(CFTimeInterval)currentTime {
