5a51e56e3b32fbd1981d0ad8997c414acb2a3c5a
Iyad Horani <i3d@ironic3d.com>
2014-07-13 18:05:50 +1000

    Initial Commit

---
+++ BreakingBricks/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  BreakingBricks
+//
+
+//  Copyright (c) 2014 IRONIC3D. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ BreakingBricks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BreakingBricks
+//
+//  Created by Iyad Horani on 13/07/2014.
+//  Copyright (c) 2014 IRONIC3D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
