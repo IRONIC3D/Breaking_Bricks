1798b4ec587c6240725fea475f3f1f6b8170ebe3
Iyad Horani <i3d@ironic3d.com>
2014-07-14 15:04:52 +1000

    Added sound on contact and added contact groups.

--- BreakingBricks/MyScene.m
+++ BreakingBricks/MyScene.m
@@ -14,28 +14,63 @@
 
 @end
 
-@implementation MyScene
+static const uint32_t ballCategory      = 0x1;      // 1 or 00000000000000000000000000000001
+static const uint32_t brickCategory     = 0x1 << 1; // 2 or 00000000000000000000000000000010
+static const uint32_t paddleCategory    = 0x1 << 2; // 4 or 00000000000000000000000000000100
+static const uint32_t edgeCategory      = 0x1 << 3; // 8 or 00000000000000000000000000001000
+
+@implementation MyScene {
+    SKAction *playBlip;
+    SKAction *playBrickHit;
+}
+
+-(void)didBeginContact:(SKPhysicsContact *)contact {
+//    if (contact.bodyA.categoryBitMask == brickCategory) {
+//        [contact.bodyA.node removeFromParent];
+//    }
+    
+    // create placeholder reference for the "non ball" object
+    SKPhysicsBody *notTheBall;
+    
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask) {
+        notTheBall = contact.bodyB;
+    } else {
+        notTheBall = contact.bodyA;
+    }
+    
+    if(notTheBall.categoryBitMask == brickCategory) {
+        [self runAction:playBrickHit];
+        [notTheBall.node removeFromParent];
+    }
+    
+    if (notTheBall.categoryBitMask == paddleCategory) {
+        
+        [self runAction:playBlip];
+    }
+}
 
 - (void)addBall:(CGSize)size {
-	// Create a new sprite node from an image
-	SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    // Create a new sprite node from an image
+    SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    
+    // Position the ball midway through the screen
+    CGPoint myPoint = CGPointMake(size.width / 2, size.height / 2);
+    ball.position = myPoint;
 	
-	// Position the ball midway through the screen
-	CGPoint myPoint = CGPointMake(size.width / 2, size.height / 2);
-	ball.position = myPoint;
+    ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width / 2];
+    ball.physicsBody.friction = 0;
+    ball.physicsBody.linearDamping = 0;
+    ball.physicsBody.restitution = 1;
+    ball.physicsBody.categoryBitMask = ballCategory;
+    ball.physicsBody.contactTestBitMask = brickCategory | paddleCategory;
 	
-	ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width / 2];
-	ball.physicsBody.friction = 0;
-	ball.physicsBody.linearDamping = 0;
-	ball.physicsBody.restitution = 1;
-	
-	// Add the sprite node to the scene
-	[self addChild:ball];
-	
-	// create the vector
-	CGVector ballKick = CGVectorMake(12, 12);
-	// apply the vector
-	[ball.physicsBody applyImpulse:ballKick];
+    // Add the sprite node to the scene
+    [self addChild:ball];
+
+    // create the vector
+    CGVector ballKick = CGVectorMake(12, 12);
+    // apply the vector
+    [ball.physicsBody applyImpulse:ballKick];
 }
 
 - (void)addBricks:(CGSize)size {
@@ -45,6 +80,7 @@
         // add a static physics body
         brick.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:brick.frame.size];
         brick.physicsBody.dynamic = NO;
+        brick.physicsBody.categoryBitMask = brickCategory;
         
         int xPos = size.width / 5 * (i+1);
         int yPos = size.height - 50;
@@ -60,6 +96,7 @@
 	self.paddle.position = CGPointMake(size.width / 2, 100);
 	self.paddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddle.frame.size];
 	self.paddle.physicsBody.dynamic = NO;
+    self.paddle.physicsBody.categoryBitMask = paddleCategory;
 	
 	[self addChild:self.paddle];
 }
@@ -68,13 +105,18 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         self.backgroundColor = [SKColor whiteColor];
+        
+        playBlip = [SKAction playSoundFileNamed:@"blip.caf" waitForCompletion:NO];
+        playBrickHit = [SKAction playSoundFileNamed:@"brickhit.caf" waitForCompletion:NO];
+        
+        // Add a physics body to the scene
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+        self.physicsBody.categoryBitMask = edgeCategory;
 		
-		// Add a physics body to the scene
-		self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
-		
-		// change gravity settings of the physics world
-		self.physicsWorld.gravity = CGVectorMake(0, -0.2);
-		
+        // change gravity settings of the physics world
+        self.physicsWorld.gravity = CGVectorMake(0, -0.2);
+        self.physicsWorld.contactDelegate = self;
+        
         [self addBall:size];
         [self addPlayer:size];
         [self addBricks:size];
