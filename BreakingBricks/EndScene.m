562fed8c69e82101616a88f50dc01825f8ac6d6d
Iyad Horani <i3d@ironic3d.com>
2014-07-14 15:44:22 +1000

    Added an end Sound, a restart scene touch with a transition.

--- BreakingBricks/EndScene.m
+++ BreakingBricks/EndScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "EndScene.h"
+#import "MyScene.h"
 
 @implementation EndScene
 
@@ -15,6 +16,10 @@
         
         self.backgroundColor = [SKColor blackColor];
         
+        SKAction *play = [SKAction playSoundFileNamed:@"gameover.caf" waitForCompletion:NO];
+        [self runAction:play];
+        
+        
         SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"Futura Medium"];
         label.text = @"You Lose!";
         label.fontColor = [SKColor whiteColor];
@@ -22,9 +27,26 @@
         label.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
         [self addChild:label];
         
+        SKLabelNode *tryAgain = [SKLabelNode labelNodeWithFontNamed:@"Futura Medium"];
+        tryAgain.text = @"tap to play again";
+        tryAgain.fontColor = [SKColor whiteColor];
+        tryAgain.fontSize = 24;
+        tryAgain.position = CGPointMake(size.width / 2, -50);
+        
+        SKAction *moveLabel = [SKAction moveToY:(size.height / 2 - 40) duration:2.0];
+        [tryAgain runAction:moveLabel];
+        
+        [self addChild:tryAgain];
+        
     }
     
     return self;
+}
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    MyScene *start = [MyScene sceneWithSize:self.size];
+    [self.view presentScene:start transition:[SKTransition doorsOpenHorizontalWithDuration:1.0]];
 }
 
 @end
